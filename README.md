6f59b83e6a174389293a4594742c7a13f361c9bb
Mark Feeney <mark.feeney@gmail.com>
2013-01-20 21:56:39 -0500

    Generates valid .class files; constructors next

--- README.md
+++ README.md
@@ -1,13 +1,63 @@
 # codegen
 
-A Clojure library designed to ... well, that part is up to you.
+Generate .class files from some simple data format.
+
+e.g.
+
+```clojure
+[{:name :Address
+  :props {:Street :String
+          :City :String
+          :Province :String}}
+ {:name :Person
+  :props {:firstName :String
+          :lastName :String
+          :age :Integer
+          :address :Address}}]
+```
+
+Produces:
+
+```
+  $ javap target/classes/com/example/gen/Address
+  public class com.example.gen.Address extends java.lang.Object{
+      public final java.lang.Object state;
+      public static {};
+      public com.example.gen.Address();
+      public java.lang.Object clone();
+      public int hashCode();
+      public java.lang.String toString();
+      public boolean equals(java.lang.Object);
+      public java.lang.String City();
+      public java.lang.String Province();
+      public java.lang.String Street();
+  }
+```
+
+```
+$ javap target/classes/com/example/gen/Person
+public class com.example.gen.Person extends java.lang.Object{
+    public final java.lang.Object state;
+    public static {};
+    public com.example.gen.Person();
+    public java.lang.Object clone();
+    public int hashCode();
+    public java.lang.String toString();
+    public boolean equals(java.lang.Object);
+    public java.lang.Integer age();
+    public java.lang.String lastName();
+    public java.lang.String firstName();
+    public com.example.gen.Address address();
+}
+```
 
 ## Usage
 
-FIXME
+* `lein compile`
+* Look at `target/classes`
 
 ## License
 
-Copyright © 2013 FIXME
+Copyright © 2013 Mark Feeney
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- src/codegen/generator.clj
+++ src/codegen/generator.clj
@@ -1,32 +1,78 @@
-(ns codegen.generator)
+(ns codegen.generator
+  "Generate a bunch of .class files from a simple data structure.  This code
+  runs entirely at compile time and is not useful at runtime (though the
+  classes it generates are theoretically useful at runtime)."
+  (:require [clojure.walk :as walk]))
+
+;; References:
+;;
+;; http://kotka.de/blog/2010/02/gen-class_how_it_works_and_how_to_use_it.html
+;; http://clojure.github.com/clojure/clojure.core-api.html#clojure.core/gen-class
+;; http://amalloy.hubpages.com/hub/Clojure-macro-writing-macros
 
 (def target-ns "com.example.gen")
 
-(def classes
-  [[:Address {:Street :String
-              :City :String
-              :Province :String}]
-   [:Person {:firstName :String
-             :lastName :String
-             :age :Int
-             :address :Address}]])
+(defn schema
+  "Returns a schema to use for class gen.  This is a fn just to demonstrate
+  that the schema could be retrieved in any way: db call, api request, file on
+  disk, etc."
+  []
+  [{:name :Address
+    :props {:Street :String
+            :City :String
+            :Province :String}}
+   {:name :Person
+    :props {:firstName :String
+            :lastName :String
+            :age :Integer
+            :address :Address}}])
 
-(defmacro gen
-  [class-sym props]
-  (let [class-name (name class-sym)
-        prefix (str class-name "-")
-        fq-class-name (str target-ns "." class-name)
-        init-fn-name (str class-name "-init")]
+;; Approach:
+;; 1) Generate implementations for each class
+;; 2) Compile these impls to .class files using gen-class
+
+(defn gen-prop-impl
+  "Generate an implementation for a single property of a class."
+  [prefix pname ptype]
+  (let [fn-name (symbol (name pname))]
+    `(defn ~fn-name [~'this] (~'.state ~pname))))
+
+(defn gen-class-impl
+  "Return code that implements all the props and constructors for cls."
+  [cls]
+  (let [fq-class-name (str target-ns "." (name (:name cls)))
+        prefix (str fq-class-name "-")
+        props (:props cls)
+        methods (vec
+                  (for [[k v] props]
+                    (let [fn-name (symbol (name k))
+                          ;; Handle when return type is a generated type
+                          return-type (if (contains? (set (map :name (schema))) v)
+                                        (symbol (str target-ns "." (name v)))
+                                        (symbol (name v)))]
+                      [fn-name [] return-type])))]
     `(do
-       ;;~props
+       ~@(for [[pname ptype] props]
+           (gen-prop-impl prefix pname ptype))
        (gen-class
-         :name ~(symbol class-name)
-         :prefix ~(symbol prefix)
-         :state ~(symbol "state") ;; bareword 'state' ends up resolved in current ns
-         :init ~(symbol init-fn-name)))))
+         :name ~fq-class-name
+         :prefix ~prefix
+         :state "state"
+         :methods ~methods))))
 
-(macroexpand-1 `(gen Address {Street String}))
+(defmacro run
+  "Generate implementations and gen-class calls for all classes described in
+  schema."
+  []
+  `(do
+     ~@(for [cls (schema)]
+         (gen-class-impl cls))))
+(comment
+  (walk/macroexpand-all '(run))
+  (macroexpand-1 '(run))
+  )
 
-;;(generate test-schema)
-;;(compile 'codegen.generator)
+(run)
+
+(compile 'codegen.generator)
 
